735d3bb20cb4f83771b0a90105f68b6542747e8b
Icaro <icaroalb1senac@gmail.com>
2025-11-24 02:44:54 -0300

    remove: markers desnecessarios

--- src/main/java/com/carework/model/Tip.java
+++ src/main/java/com/carework/model/Tip.java
@@ -1,11 +1,9 @@
 package com.carework.model;
 
 import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import jakarta.persistence.Table;
 import jakarta.persistence.PrePersist;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 import java.util.UUID;
